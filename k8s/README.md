dbe9bfe665be29b8e7f3df10550f58e7299a19d6
suhavni <suhavni@gmail.com>
2022-06-06 17:31:35 +0700

    extract image before gifify for option 1 and create redis-related kubernetes yaml files

---
+++ k8s/README.md
@@ -0,0 +1,4 @@
+Before doing `kubectl apply -f .`, please create the namespace for redis via the following command:
+```
+kubectl add ns redis
+```
--- script/src/main.py
+++ script/src/main.py
@@ -3,7 +3,6 @@ from utility import *
 
 FUNCTIONS = {
     'generate_thumbnail' : generate_thumbnail,
-    'extract_images' : extract_images,
     'extract_and_gifify' : extract_and_gifify
 }
 
@@ -12,7 +11,7 @@ def main():
     if len(argv) < 4:
         print('Too little arguments were given', sys.stderr) 
         return
-    func = FUNCTIONS.get(argv[1])
+    func = FUNCTIONS.get(argv[1], generate_thumbnail)
     func(in_filename=argv[2], out_filename=argv[3])
 
 if __name__ == '__main__':
