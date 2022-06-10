d5dc94932b3ae9ab48c0e9ec0ec22422a8e59a01
sudo-lucifer <krittin.niu@student.mahidol.edu>
2022-06-10 22:48:24 +0700

    Create submit job to work queue api

---
+++ web-controller/web_controller/apis/submit_job_api.py
@@ -0,0 +1,26 @@
+from flask import request, jsonify
+from flask import current_app as app
+from itsdangerous import json
+from web_controller import db, minio_client
+from web_controller.model import Job
+from web_controller.utils.work_queue import send_to_worker
+
+
+@app.route("/submit-job", methods=['POST'])
+def submit_job():
+	job_detail = request.json
+	# serializer-like
+	if (job_detail is None):
+		return jsonify(error={"title": "Json body is required"})
+	elif (job_detail.get("input_file", None) is None):
+		return jsonify(error={"input_file": "this field is required"})
+	elif (job_detail.get("output_file", None) is None):
+		return jsonify(error={"output_file": "this field is required"})
+	# submit job to work queue
+	# TODO: need job id from send_to_work function
+	worker_id = send_to_worker(job_detail)
+
+	# TODO: update to database
+	# ....
+
+	return jsonify(job_id=worker_id)
