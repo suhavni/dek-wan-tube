74d835c7d20022d2f9def74c2b760be363368b76
sudo-lucifer <krittin.niu@student.mahidol.edu>
2022-06-10 22:23:56 +0700

    Update virutal env

---
+++ web-controller/web_controller/apis/__init__.py
@@ -0,0 +1,1 @@
+from web_controller import minio
--- web-controller/web_controller/utils/work_queue.py
+++ web-controller/web_controller/utils/work_queue.py
@@ -1,8 +1,8 @@
 from flask import request
 from worker_utility import RedisResource, worker
 
-def send_to_worker():
-    body = request.json
+def send_to_worker(body):
+    # body = request.json
     in_filename = body.get('input_file', 'input.mp4')
     out_filename = body.get('output_file', 'output.mp4')
 
