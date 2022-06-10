0bec690b65736424d0fd82e1fe9c01969ef9c2cc
suhavni <suhavni@gmail.com>
2022-06-11 00:38:25 +0700

    edit typo (forgot to change worker function name accordingly when called)

--- web-controller/web_controller/utils/work_queue.py
+++ web-controller/web_controller/utils/work_queue.py
@@ -1,9 +1,9 @@
 from flask import request
-from worker_utility import RedisResource, worker
+from worker_utility import RedisResource, extract_worker
 
 def send_to_worker(body):
     # body = request.json
     in_filename = body.get('input_file', 'input.mp4')
     out_filename = body.get('output_file', 'output.mp4')
 
-    RedisResource.extract_queue.enqueue_call(worker, args=[in_filename, out_filename])
+    RedisResource.extract_queue.enqueue_call(extract_worker, args=[in_filename, out_filename])
