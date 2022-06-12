7aa048c80e95e6be20edb79ef27744f9e38b2f20
suhavni <suhavni@gmail.com>
2022-06-13 00:15:15 +0700

    more changes to prev commit

--- web-controller/web_controller/utils/worker_utility.py
+++ web-controller/web_controller/utils/worker_utility.py
@@ -1,24 +1,10 @@
 import os
 import logging
-from redis import Redis
-from rq import Queue
 import subprocess
 import sys
-# import web_controller
+from . import RedisResource
 
 LOG = logging
-
-class RedisResource:
-    REDIS_QUEUE_LOCATION = os.getenv('REDIS_QUEUE', 'localhost')
-    host, *port_info = REDIS_QUEUE_LOCATION.split(':')
-    port = tuple()
-    if port_info:
-        port, *_ = port_info
-        port = (int(port),)
-
-    conn = Redis(host=host, *port)
-    extract_queue = Queue('extract', connection=conn)
-    composer_queue = Queue('composer', connection=conn)
 
 def extract_worker(in_filename, out_filename):
     # FIXME: make the file work such that it takes input from MinIO and outputs into MinIO
