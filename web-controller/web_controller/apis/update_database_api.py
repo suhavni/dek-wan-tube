c6bcb80b698e7ca1d50aa57e8b976d650425058f
sudo-lucifer <krittin.niu@student.mahidol.edu>
2022-06-13 00:21:15 +0700

    Add api for updating job database

---
+++ web-controller/web_controller/apis/update_database_api.py
@@ -0,0 +1,21 @@
+from flask import request, jsonify
+from flask import current_app as app
+from web_controller import *
+from web_controller.model import Job
+
+
+@app.route("/api/update-database", methods=['POST'])
+def update_database():
+	job_detail = request.json
+	# serializer-like
+	if (job_detail is None):
+		return jsonify(error={"title": "Json body is required"})
+	elif (job_detail.get("id", None) is None):
+		return jsonify(error={"id": "this field is required"})
+	elif (job_detail.get("status", None) is None):
+		return jsonify(error={"status": "this field is required"})
+	# submit job to work queue
+	job = Job.query.get(int(job_detail.get("id")))
+	job.status = job_detail.get("status")
+
+	return jsonify(status="Updated", id=int(job_detail.get("id")))
