53c59551505b4081de5bc645d38a4cc11f85ec6f
suhavni <suhavni@gmail.com>
2022-06-13 03:56:34 +0700

    connect to extract to minio

--- web-controller/web_controller/utils/__init__.py
+++ web-controller/web_controller/utils/__init__.py
@@ -1,6 +1,7 @@
 import os
 from redis import Redis
 from rq import Queue
+from .. import MinioConnect
 
 class RedisResource:
     REDIS_QUEUE_LOCATION = os.getenv('REDIS_QUEUE', 'localhost')
@@ -13,4 +14,34 @@ class RedisResource:
     conn = Redis(host=host, *port)
     extract_queue = Queue('extract', connection=conn)
     composer_queue = Queue('composer', connection=conn)
-    update_status_queue = Queue('update_status', connection=conn)
+    update_status_queue = Queue('update_status', connection=conn, is_async=False)
+
+class MinioUpdate:
+    minio_client = MinioConnect.minio_client
+
+    def download_file(self, bucket_name, file_name, job_id):
+        self.minio_client.fget_object(
+            bucket_name,
+            file_name,
+            f'/tmp/{job_id}/{file_name}'
+        )
+
+    def add_folder(self, bucket_name, folder_name):
+        self.minio_client.make_bucket(bucket_name)
+        self.minio_client.make_bucket(f'{bucket_name}/{folder_name}')
+
+    def upload_file(self, bucket_name, file_name, job_id):
+        self.minio_client.fput_object(
+            bucket_name,
+            file_name,
+            f'/tmp/{job_id}/{file_name}'
+        )
+
+    def delete_file(self, bucket_name, file_name):
+        self.minio_client.remove_object(
+            bucket_name,
+            file_name
+        )
+    
+    def delete_folder(self, bucket_name, folder_name):
+        self.minio_client.remove_bucket(f'{bucket_name}/{folder_name}')
--- web-controller/web_controller/utils/worker_utility.py
+++ web-controller/web_controller/utils/worker_utility.py
@@ -1,12 +1,15 @@
 import os
 import subprocess
 import sys
-from . import RedisResource
+from . import RedisResource, MinioUpdate
+
+MINIO_UPDATE = MinioUpdate()
 
 def extract_worker(in_filename, out_filename, job_id):
-    import requests
-    # FIXME: make the file work such that it takes input from MinIO and outputs into MinIO
-    process = subprocess.Popen(f'python ../resources/main.py extract {in_filename} {out_filename}', shell=True)
+    RedisResource.update_status_queue.enqueue_call(update_status_worker, args=[job_id, "Downloading video"])
+    MINIO_UPDATE.download_file('video', in_filename, job_id)
+    
+    process = subprocess.Popen(f'python ../resources/main.py extract tmp/{job_id}/{in_filename} tmp/{job_id}/{out_filename}', shell=True)
     RedisResource.update_status_queue.enqueue_call(update_status_worker, args=[job_id, "Started extracting frames"])
 
     return_code = process.wait()
@@ -18,7 +21,7 @@ def extract_worker(in_filename, out_filename, job_id):
         RedisResource.update_status_queue.enqueue_call(update_status_worker, args=[job_id, f"There was an error while extracting frames. Exit code: {return_code}"])
     
 def compose_worker(in_filename, out_filename, job_id):
-    process = subprocess.Popen(f'python ../resources/main.py gif_composer {in_filename} {out_filename}', shell=True)
+    process = subprocess.Popen(f'python ../resources/main.py gif_composer tmp/{job_id}/{in_filename} tmp/{job_id}/{out_filename}', shell=True)
     RedisResource.update_status_queue.enqueue_call(update_status_worker, args=[job_id, "Started composing GIF"])
 
     return_code = process.wait()
@@ -29,4 +32,5 @@ def compose_worker(in_filename, out_filename, job_id):
         RedisResource.update_status_queue.enqueue_call(update_status_worker, args=[job_id, f"There was an error while composing GIF. Exit code: {return_code}"])
      
 def update_status_worker(job_id, status):
+    import requests
     requests.post(f'http://localhost:5000/api/update-database', json={'id': job_id, 'status': status})
