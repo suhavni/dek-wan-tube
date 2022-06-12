1268e39a9db1665fc5cbf7b2f95dde23f8618afb
sudo-lucifer <krittin.niu@student.mahidol.edu>
2022-06-12 23:19:39 +0700

    Fix job database connection

--- web-controller/config.py
+++ web-controller/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-	SQLALCHEMY_DATABASE_URI = os.getenv("FLASK_DATABASE_URI")
+	SQLALCHEMY_DATABASE_URI = os.getenv("FLASK_DATABASE_URI", "mariadb+pymysql://saobangpho:scp1234@localhost:3306/scalable-p2-jobs")
 	SQLALCHEMY_ECHO = True
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
