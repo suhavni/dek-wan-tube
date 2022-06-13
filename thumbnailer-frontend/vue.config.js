a8b6fa70739b41c9252137c6e53be3c7401d4b92
sudo-lucifer <krittin.niu@student.mahidol.edu>
2022-06-13 21:17:15 +0700

    Add vue axios, configure proxy

--- thumbnailer-frontend/vue.config.js
+++ thumbnailer-frontend/vue.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+  devServer: {
+    host: "0.0.0.0",
+    disableHostCheck: true,
+    port: 9000,
+    https: false,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+      },
+    },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
+  },
+
 };
