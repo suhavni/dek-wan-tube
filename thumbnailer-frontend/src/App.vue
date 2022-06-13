e2aa8520dd166bfe1b967964043a09c7728c078e
sudo-lucifer <krittin.niu@student.mahidol.edu>
2022-06-13 22:10:15 +0700

    Add pages, create navigation drawer, configure router

--- thumbnailer-frontend/src/App.vue
+++ thumbnailer-frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-app-bar app color="#2E7D32" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -21,17 +21,46 @@
         />
       </div>
 
-      <v-spacer></v-spacer>
+      <!--      <v-spacer></v-spacer>-->
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <!--      <v-btn-->
+      <!--        href="https://github.com/vuetifyjs/vuetify/releases/latest"-->
+      <!--        target="_blank"-->
+      <!--        text-->
+      <!--      >-->
+      <!--        <span class="mr-2">Latest Release</span>-->
+      <!--        <v-icon>mdi-open-in-new</v-icon>-->
+      <!--      </v-btn>-->
     </v-app-bar>
+    <v-navigation-drawer color="#E8F5E9" permanent expand-on-hover absolute>
+      <v-list-item class="mt-16">
+        <v-list-item-content>
+          <v-list-item-title class="text-h6 mt-5" style="color: #37474f"
+            >Menu</v-list-item-title
+          >
+          <v-list-item-subtitle> Select any choice </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          :to="item.link"
+        >
+          <v-list-item-icon>
+            <v-icon color="#546E7A">{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
       <router-view />
@@ -44,7 +73,16 @@ export default {
   name: "App",
 
   data: () => ({
-    //
+    items: [
+      {
+        title: "Control Center",
+        icon: "mdi-application-cog-outline",
+        link: "/",
+      },
+      { title: "GIF Files", icon: "mdi-image", link: "/gif-files" },
+      { title: "Jobs", icon: "mdi-account-hard-hat", link: "/jobs" },
+    ],
+    right: null,
   }),
 };
 </script>
