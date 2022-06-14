b5bc54e2b48e45d5170d1447d235f7661bd3f0c0
sudo-lucifer <krittin.niu@student.mahidol.edu>
2022-06-15 03:18:37 +0700

    Add list all video with url API in web controller

---
+++ web-controller/web_controller/apis/video_list_api.py
@@ -0,0 +1,42 @@
+from datetime import timedelta
+from flask import request, jsonify
+from flask import current_app as app
+from web_controller import MinioConnect
+import mimetypes
+mimetypes.init()
+
+
+@app.route("/api/video-list", methods=['POST'])
+def list_all_videos():
+	# submit job to work queue
+	bucket = request.json
+	# serializer-like
+	if (bucket is None or bucket.get("bucket_name",None) is None):
+		return jsonify(error={"bucket_name": "this field is required"})
+
+	request_bucket_name = bucket.get("bucket_name")
+	minio_connector = MinioConnect.minio_client
+	# check if bucket exists
+	if (not minio_connector.bucket_exists(request_bucket_name)):
+		return jsonify(title=f"Bucket {request_bucket_name} not found")
+	# list all files in bucket
+	bucket_objects = minio_connector.list_objects(request_bucket_name, recursive=True)
+	videos = []
+	for file in bucket_objects:
+		file_name = file.object_name
+		mimetype = mimetypes.guess_type(file_name)[0]
+		if (mimetype):
+			mimetype = mimetype.split('/')[0]
+			if (mimetype == 'video'):
+				url = minio_connector.presigned_get_object(
+					bucket_name=request_bucket_name,
+					object_name=file_name,
+					expires=timedelta(hours=24)
+				)
+				videos.append(
+					{
+						"name": file_name,
+						"video_url": url
+					}
+				)
+	return jsonify(videos=videos)
