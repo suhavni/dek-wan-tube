b9e07b44874b29d9fb1069342046f10eb93861a5
suhavni <suhavni@gmail.com>
2022-06-15 04:17:37 +0700

    need k8s testing for gif-file-page

--- thumbnailer-frontend/src/views/GifFilesPage.vue
+++ thumbnailer-frontend/src/views/GifFilesPage.vue
@@ -5,14 +5,14 @@
         <v-row>
           <h2>All GIFs</h2>
           <v-spacer></v-spacer>
-          <v-btn outlined color="#81a36f" class="justify-end">
+          <v-btn outlined color="#81a36f" class="justify-end" @click.stop="dialog = true">
             Delete All
             <v-icon right dark> mdi-trash-can-outline </v-icon>
           </v-btn>
         </v-row>
       </div>
       <v-divider class="mt-5"></v-divider>
-      <v-container class="pa-0 mt-3">
+      <v-container class="pa-0 mt-3" v-if="gifs.length !== 0">
         <v-row v-for="i in Math.ceil(gifs.length / 3)" :key="i">
           <v-col v-for="j in 3" :key="j">
             <gif-card
@@ -21,8 +21,55 @@
             ></gif-card>
           </v-col>
         </v-row>
+        <v-dialog
+            v-model="dialog"
+            max-width="290"
+        >
+          <v-card>
+            <v-card-title>
+              Delete All GIFs?
+            </v-card-title>
+
+            <v-card-text>
+              Are you sure you want to delete all the GIF files?
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn
+                  color="#81a36f"
+                  text
+                  @click="dialog = false"
+              >
+                Disagree
+              </v-btn>
+
+              <v-btn
+                  color="#81a36f"
+                  text
+                  @click="deleteAllGIFs()"
+              >
+                Agree
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-container>
+      <v-container v-else>
+        <v-card color="#00000000" elevation="0">
+          <v-card-title class="justify-center">No GIFs Found</v-card-title>
+          <v-card-text class="text-center">Why don't you submit a new job?</v-card-text>
+          <v-row  no-gutters justify="space-around">
+            <v-img
+              src="https://www.roots.tech/web/image/13846-cb303eb9/No%20data-cuate.png"
+              max-width="370"
+            />
+          </v-row>
+        </v-card>
       </v-container>
     </v-container>
+    {{gifs.length}}
   </div>
 </template>
 
@@ -34,11 +81,19 @@ export default {
   components: { GifCard },
   data: () => ({
     gifs: [],
+    dialog: false,
   }),
   methods: {
     async getAllGIFs() {
-      const data = { "bucket_name" : "video" }
-      this.gifs = await Vue.axios.get("/api/gif-list", data)
+      const data = { "bucket_name" : "gif" }
+      this.gifs = (await Vue.axios.post("/api/gif-list", data)).data.file_names;
+    },
+    async deleteAllGIFs() {
+      const data = {
+        "bucket_name": "gif",
+      }
+      await Vue.axios.post('api/delete-all-gifs', data);
+      window.location.reload();
     }
   },
   created() {
